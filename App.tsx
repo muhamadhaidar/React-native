23198605b2274513a8dfbd9342d9df2a0a4f5016
muhamadhaidar <145978283+muhamadhaidar@users.noreply.github.com>
2025-10-01 21:25:32 +0700

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Text, View, StyleSheet, StatusBar, useColorScheme } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+function App() {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  return (
+    <SafeAreaProvider>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <View style={styles.container}>
+        <Text style={styles.text}>Selamat datang di react native</Text>
+      </View>
+    </SafeAreaProvider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f5f5f5',
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#333',
+  },
+});
+
+export default App;
